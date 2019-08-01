694daa1ffffd253d44549f329f0cfec1ad87d08f
Daniel Gleason <damianscape@gmail.com>
2019-08-01 17:03:34 -0400

    user system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+
+  resources :sessions, only: %i[new create destroy]
+
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
   root 'home#index'
 end
