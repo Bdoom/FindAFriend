6f2be069e534367fc28b70ca08e12b57f09f83c6
Daniel Gleason <damianscape@gmail.com>
2019-09-10 17:22:42 -0400

    about to add videos.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 Recaptcha.configure do |config|
-    config.site_key = '6LfdcbYUAAAAADUDL_Shtex6SbaVQ-SHr5nLgvDA'
-    config.secret_key = '6LfdcbYUAAAAAJ4pHqWM9wYvPYqZqPAAhRhMgof3'
+  config.site_key = '6LfdcbYUAAAAADUDL_Shtex6SbaVQ-SHr5nLgvDA'
+  config.secret_key = '6LfdcbYUAAAAAJ4pHqWM9wYvPYqZqPAAhRhMgof3'
 end
-  
