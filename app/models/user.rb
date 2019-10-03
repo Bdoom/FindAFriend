8081fd42bf5b42612d7bc8192accd1a96f658e5f
Daniel Gleason <damianscape@gmail.com>
2019-10-03 19:28:55 -0400

    test4

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,9 +26,9 @@ class User < ApplicationRecord
       user.refresh_token = auth.credentials.refresh_token
       user.first_name = 'Unknown'
       user.last_name = 'Name'
-      user.password = Devise.friendly_token[0, 20]
-      user.skip_confirmation!      
-      user.update_without_password
+      #user.password = Devise.friendly_token[0, 20]
+      user.skip_confirmation!
+      user.save!
     end
   end
 
