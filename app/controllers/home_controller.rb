09f96a90c30eacdababd6e6106546d3ac0d3e559
Daniel <damianscape@gmail.com>
2019-10-28 11:50:17 -0400

    added meta tags to main page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
     if current_user != nil
       redirect_to user_path(current_user)
     else
+      @page_title       = 'Login or Sign up'
+      @page_description = 'Connect with friends, make new friends, and share photos.'
+
       render 'home/index'
     end
   end
